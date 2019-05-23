0638cf6f71153d21c51378b99fe5dc6e2fb2bad7
Sreejithperfect <50919198+Sreejithperfect@users.noreply.github.com>
2019-05-23 22:42:48 +0530

    Update 40.py

--- 40.py
+++ 40.py
@@ -4,14 +4,17 @@ v=0
 n1 = 1
 n2 = 1
 count = 0
-if a<= 0:
-    v=0
-elif a == 1:
-    v=0
+if(a==1):
+    print("1")
 else:
-   while count < a:
-       print(n1,end=" ")
-       nth = n1 + n2
-       n1 = n2
-       n2 = nth
-       count += 1
+    if a<= 0:
+        v=0
+    elif a == 1:
+        v=0
+    else:
+        while count < a:
+            print(n1,end=" ")
+            nth = n1 + n2
+            n1 = n2
+            n2 = nth
+            count += 1
