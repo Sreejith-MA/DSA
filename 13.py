0e400e7f3dca4443c8dca7cab7c2fb2daa8120ea
Sreejithperfect <50919198+Sreejithperfect@users.noreply.github.com>
2019-05-23 16:54:18 +0530

    Create 13.py

---
+++ 13.py
@@ -0,0 +1,12 @@
+a = int(input())  
+c=0 
+if a > 1:  
+   for i in range(2,a):  
+       if (a % i) == 0:
+           print("no")
+           break
+       else:  
+            print("yes")
+            break
+else:
+    print("no")
