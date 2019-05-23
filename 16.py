67dd1445cf0372c3e39b437422544dea357ae2ec
Sreejithperfect <50919198+Sreejithperfect@users.noreply.github.com>
2019-05-23 18:01:28 +0530

    Create 16.py

---
+++ 16.py
@@ -0,0 +1,15 @@
+a=input()
+a=a.split(" ")
+c=int(a[0])
+d=int(a[1])
+e=[]
+if (c<2):
+    print("2",end=" ")
+for i in range(c,d+1):  
+   if i > 1:  
+       for j in range(2,i):  
+           if (i % j) == 0:  
+               break  
+       else:  
+           if(i%2!=0):
+            print(i,end=" ")
