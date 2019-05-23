73be5a4e4035adfbcf53f10c3f8ed24e5e716724
Sreejithperfect <50919198+Sreejithperfect@users.noreply.github.com>
2019-05-23 21:08:14 +0530

    Update 27.py

--- 27.py
+++ 27.py
@@ -1,5 +1,18 @@
 a=input()
-if a.isdigit() == True:
+d=0
+c=len(a)
+if (ord (a[0])<57):
+    if(ord (a[0])>47 or ord (a[0])==45 or ord (a[0])== 46):
+        for i in range (1,c):
+            if(ord (a[i])>43 and ord (a[i])<58):
+                d=1
+            else:
+                d=d-1
+    else:
+        d=d-1
+else:
+    d=d-1
+if (d>0):
     print("yes")
 else:
     print("no")
