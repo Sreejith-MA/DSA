f235aa7388478a6419b49d100aea3e7532791f92
Sreejithperfect <50919198+Sreejithperfect@users.noreply.github.com>
2019-05-23 20:48:23 +0530

    Update 26.py

--- 26.py
+++ 26.py
@@ -1,7 +1,7 @@
-c=int(input())
-a=input()
-a=a.split(' ')
-v= [int(i) for i in a]
-v.sort()
+a=int(input())
+s=input()
+s=s.split(' ')
+c= [int(i) for i in s]
+c.sort()
 for i in range (c):
-    print(v[i],end=" ")
+    print(c[i],end=" ")
