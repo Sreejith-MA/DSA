b45253b5eb5657dee6705cca80a393c102fbd0fc
Sreejithperfect <50919198+Sreejithperfect@users.noreply.github.com>
2019-05-23 22:38:25 +0530

    Create 40.py

---
+++ 40.py
@@ -0,0 +1,17 @@
+
+a=int(input())
+v=0
+n1 = 1
+n2 = 1
+count = 0
+if a<= 0:
+    v=0
+elif a == 1:
+    v=0
+else:
+   while count < a:
+       print(n1,end=" ")
+       nth = n1 + n2
+       n1 = n2
+       n2 = nth
+       count += 1
